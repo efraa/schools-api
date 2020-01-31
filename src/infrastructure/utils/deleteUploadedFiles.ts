02486f45737578ecb39c163094963bb6baae73d6
Efra Peralta <efra23peralta@gmail.com>
2020-01-31 14:14:43 -0400

    fix: uploads path

--- src/infrastructure/utils/deleteUploadedFiles.ts
+++ src/infrastructure/utils/deleteUploadedFiles.ts
@@ -2,6 +2,6 @@ import fs from 'fs'
 import path from 'path'
 
 export const deleteUploadedFiles = async (imageRoute: string) => {
-  const imagePath = path.resolve(__dirname, `../../../uploads/${imageRoute}`)
+  const imagePath = path.resolve(`uploads/${imageRoute}`)
   if (fs.existsSync(imagePath)) await fs.unlinkSync(imagePath)
 }
