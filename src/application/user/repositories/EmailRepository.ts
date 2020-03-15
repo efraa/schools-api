a10d6ae17cb7fd75ebccc4a3db661b4972c4183f
Efra Peralta <efra23peralta@gmail.com>
2020-03-15 04:15:55 -0400

    feat: add email repository

--- src/application/user/repositories/EmailRepository.ts
+++ src/application/user/repositories/EmailRepository.ts
@@ -1,11 +1,21 @@
-import { Repository, EntityRepository } from 'typeorm'
+import { Repository, EntityRepository, MoreThanOrEqual } from 'typeorm'
 import { Email } from '../../../database/entities/Email'
 
 @EntityRepository(Email)
 export class EmailRepository extends Repository<Email> {
-  public get = async (email: string): Promise<Email|undefined> =>
+  public get = async (email: string) =>
     await this.manager.getRepository(Email).findOne({ email })
 
-  public updateEmail = async (email: Email, updates: any): Promise<Email|undefined> =>
+  public updateEmail = async (email: Email, updates: any) =>
     await this.manager.getRepository(Email).merge(email, updates)
+
+  public getEmailWithCode = async (email: string, code: number) =>
+    await this.manager.getRepository(Email).findOne({
+      email,
+      code,
+      expire: MoreThanOrEqual(new Date())
+    })
+
+  public deleteByEmail = async (email: string) =>
+    await this.manager.getRepository(Email).delete({ email })
 }
