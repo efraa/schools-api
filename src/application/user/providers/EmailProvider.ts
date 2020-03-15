85d55f2e339e9ffbe24a81631a841400d4e22f22
Efra Peralta <efra23peralta@gmail.com>
2020-03-14 20:27:11 -0400

    feat(email): add email provider

---
+++ src/application/user/providers/EmailProvider.ts
@@ -0,0 +1,4 @@
+export { EmailService } from '../services/EmailService'
+export { EmailRepository } from '../repositories/EmailRepository'
+export { EmailDTO } from '../domain/dtos/EmailDTO'
+export { EmailMapper } from '../domain/mappers/EmailMapper'
