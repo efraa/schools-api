48548041922b39cb067d40cd283513f155c98022
Efra Peralta <efra23peralta@gmail.com>
2020-03-15 04:17:03 -0400

    feat: add new responses

--- src/application/user/utils/UserResponses.ts
+++ src/application/user/utils/UserResponses.ts
@@ -7,7 +7,8 @@ export const UserResponses = {
     COMMON_PASSWORD: 'Oops!, your password is not safe, try using not so common words and numbers.',
     EMAIL_OR_USERNAME: 'Please include a valid username or email.',
     TOKEN: 'Token is required',
-    ROLE: 'Please, include a valid role.'
+    ROLE: 'Please, include a valid role.',
+    CODE: 'Please, include a valid code',
   },
   USER_NOT_FOUND: 'Oops! We did not find this user in the organization.',
   ACCOUNT_NOT_FOUND: 'Oops! There is no user with these credentials.',
@@ -26,5 +27,7 @@ export const UserResponses = {
   EMAIL_SENT: 'An email has been sent to your account.',
   FORGOT_PASS_CHANGED: 'Your password has been changed successfully.',
   INVALID_FILE_EXT: 'Oops! The file or its extension is incorrect.',
-  EMAIL_MANY_REQUEST_ATTEMPTS: 'Oops! This email has too many requests, it has been blocked for 24 hours.'
+  EMAIL_MANY_REQUEST_ATTEMPTS: 'Oops! This email has too many requests, it has been blocked for 24 hours.',
+  EMAIL_VERIFY_CODE: 'This code is not valid',
+  EMAIL_NOT_FOUND: 'Oops! We did not find this email.',
 }
