2ae98463118b3eb98e238e79e26a09e339849709
Efra Peralta <efra23peralta@gmail.com>
2020-03-17 23:52:44 -0400

    feat(school): add validators

---
+++ src/application/school/utils/SchoolValidators.ts
@@ -0,0 +1,17 @@
+import { check, param } from 'express-validator'
+import { SchoolResponses } from './SchoolResponses'
+
+const { validator } = SchoolResponses
+
+const create = [
+  check('name', validator('name')).isLength({ min: 3 }),
+  check('dean', validator('dean')).optional().isLength({ min: 3 }),
+  check('slogan', validator('slogan')).optional().isLength({ min: 3 }),
+  check('type', validator('type')).isLength({ min: 3 }),
+  check('zone', validator('zone')).isLength({ min: 3 }),
+  check('website', validator('website', 5)).optional().isLength({ min: 5 }),
+]
+
+export const validators = {
+  create,
+}
